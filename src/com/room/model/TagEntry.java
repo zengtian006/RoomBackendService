50ad8d83d752985c25fdee72b2ced9639a3364d4
Zeng <zengtian006@gmail.com>
2017-03-07 10:37:57 +0800

    tim

--- src/com/room/model/TagEntry.java
+++ src/com/room/model/TagEntry.java
@@ -1,28 +1,37 @@
 package com.room.model;
 
-public class TagEntry {
-	private String key;
-    private Long value;
+import java.io.Serializable;
 
-    public TagEntry(String key, Long value) {
+public class TagEntry implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String key;
+	private Long value;
+
+	public TagEntry() {
+	}
+
+	public TagEntry(String key, Long value) {
 		super();
 		this.key = key;
 		this.value = value;
 	}
 
 	public String getKey() {
-        return key;
-    }
+		return key;
+	}
 
-    public void setKey(String key) {
-        this.key = key;
-    }
+	public void setKey(String key) {
+		this.key = key;
+	}
 
-    public Long getValue() {
-        return value;
-    }
+	public Long getValue() {
+		return value;
+	}
 
-    public void setValue(Long value) {
-        this.value = value;
-    }
+	public void setValue(Long value) {
+		this.value = value;
+	}
 }
